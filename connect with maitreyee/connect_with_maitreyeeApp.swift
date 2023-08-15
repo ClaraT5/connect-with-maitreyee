af344cae0e5f826d9ba81eecbd706bc766d95cea
Clara B <claratamsin5@gmail.com>
2023-08-15 13:26:43 -0400

    Initial Commit

---
+++ connect with maitreyee/connect_with_maitreyeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  connect_with_maitreyeeApp.swift
+//  connect with maitreyee
+//
+//  Created by Clara T Brown on 2023-08-15.
+//
+
+import SwiftUI
+
+@main
+struct connect_with_maitreyeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
